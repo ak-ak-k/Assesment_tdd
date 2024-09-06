163c23eff216625c1f475d1934c5506d559a9895
aditya <adityakirtane27@gmail.com>
2024-09-06 12:39:27 +0530

    Final commit -- Clean up and organize code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,5 +14,30 @@ export class AppComponent {
 
   ngOnInit(): void {}
 
-  calculate() {}
+  calculate() {
+    this.error = '';
+    let delimiter = /,|\n/;
+    let numbers = this.input;
+
+    if (numbers.startsWith('//')) {
+      const delimiterEndIndex = numbers.indexOf('\n');
+      delimiter = new RegExp(numbers.substring(2, delimiterEndIndex));
+      numbers = numbers.substring(delimiterEndIndex + 1);
+    }
+
+    const numArray = numbers.split(delimiter).map((n) => parseInt(n, 10));
+
+    const negativeNumbers = numArray.filter((num) => num < 0);
+    if (negativeNumbers.length > 0) {
+      this.error = `Negatives not allowed: ${negativeNumbers.join(', ')}`;
+      this.result = 0;
+      return;
+    }
+
+    // Sum the numbers and ignore NaN values (for non-numeric parts)
+    this.result = numArray.reduce(
+      (accum, num) => accum + (isNaN(num) ? 0 : num),
+      0
+    );
+  }
 }
